6cd06b2a6c9c74341939746de7aec946379f53b5
developkariyer <develop@kariyerfora.com>
2024-11-14 22:07:37 +0100

    a

--- slack.py
+++ slack.py
@@ -3,7 +3,7 @@ import requests
 from env_secrets import SLACK_WEBHOOK_URL
 
 def send_message_to_channel(text, thread_ts):
-    payload = {"text": text, "thread_ts": thread_ts}
+    payload = {"text": text} #, "thread_ts": thread_ts}
     headers = {'Content-Type': 'application/json'}
     response = requests.post(SLACK_WEBHOOK_URL, headers=headers, data=json.dumps(payload))
     if response.status_code != 200:
